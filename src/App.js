2a4f8f682ebb170f673883041656ee18a6c7b336
Sulaiman <skshanwari830@gmail.com>
2021-10-12 17:08:53 +0500

    updated app.js.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
-import Shops  from './Shops/Shops';
+import Shops from './Shops/Shops';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Header />
-       <Shops />
-       <Footer />
+        <Header />
+        {/* <Shops /> */}
+        <h3>main component placeholder</h3>
+        <Footer />
       </div>
     );
   }
