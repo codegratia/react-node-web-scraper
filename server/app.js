abd6094786f629f72808c8707cde4a8f698edb9f
Sulaiman <sulaimanshinwari830@gmail.com>
2023-10-27 15:13:42 +0500

    Added api end point for getting products data from files inside data directory

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 const fs = require("fs");
+const path = require("path");
 
 const scrapeDynamicWebpage = require("./scrapers/dynamincSiteScraper");
 const scrapeStaticWebpage = require("./scrapers/staticSiteScraper");
@@ -12,19 +13,32 @@ const scrapeStaticWebpage = require("./scrapers/staticSiteScraper");
 var folder = "./data";
 
 if (!fs.existsSync(folder)) {
- fs.mkdirSync(folder);
- console.log("Folder Created Successfully.");
+  fs.mkdirSync(folder);
+  console.log("Folder Created Successfully.");
 }
 app.get("/", function (req, res) {
- res.send("Happy web scraping");
+  res.send("Happy web scraping");
 });
 
-app.get("/items", function (req, res) {
- res.json(data);
+app.get("/api/v1/items", function (req, res) {
+  fs.readdir("./data", (err, files) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send(err);
+    } else {
+      let data = {};
+      files.forEach((file) => {
+        const filePath = path.join("./data", file);
+        const fileData = JSON.parse(fs.readFileSync(filePath, "utf8"));
+        data[file] = fileData;
+      });
+      res.json(data);
+    }
+  });
 });
 
 app.listen(PORT, () => {
- console.log("Server running on port " + PORT);
+  console.log("Server running on port " + PORT);
 });
 
 // scrapeDynamicWebpage();
